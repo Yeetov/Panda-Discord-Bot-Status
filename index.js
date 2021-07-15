a16aa7592b4f10e6d9f81a4be20a7b5f0f7057bd
FatalFang <>
2021-07-15 12:05:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity("PandaTV", { type: "WATCHING" })
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	if (interaction.commandName === 'ping') {
+		await interaction.reply('Pong!');
+	}
+});
+
+client.login(process.env.TOKEN);
